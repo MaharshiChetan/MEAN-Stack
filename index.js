70ebca125bcef90f57161ea653e2d5b267baecd5
MaharshiChetan <chetanmahabboy@gmail.com>
2018-04-28 10:47:35 +0530

    server-setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const config = require("./config/database");
+const path = require("path");
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri, (err) => {
+   if (err) {
+      console.log("Could NOT connect to the database: ", err);
+   } else {
+      console.log("Connected to the database: " + config.db);
+   }
+});
+
+app.use(express.static(__dirname + "/client/dist/"));
+
+app.get("*", (req, res) => {
+   res.sendFile(path.join(__dirname + "/client/dist/index.html"));
+})
+
+app.listen(3000, () => console.log("App running on port 3000"));
